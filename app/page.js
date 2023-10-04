ee8ecbe46e9c44dc6027446a96626d932a1afe95
Rithikeshh <rithikeshh@gmail.com>
2023-10-04 15:34:00 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
-import React from "react"
-
+'use client'
+import React,{useReducer} from "react"
+import reducer from './TaskReducer'
 export default function Home() {
+  const [taskState, dispatch] = useReducer(reducer,{count : 0})
   return (
     <div id="main">
       <h2>Task Counter</h2>
-      {/*<button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
+      <button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
       <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
       <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
-  <h3 className="counter">Number of Tasks : {taskState.count}</h3>*/}
+  <h3 className="counter">Number of Tasks : {taskState.count}</h3>
 
     {/*Update the above code to achieve the solution*/}
     </div>
